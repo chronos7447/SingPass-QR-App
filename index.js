d22dedbac65c50976f7c9b72aa6c3b79b56b389c
chronos7447 <35457427+chronos7447@users.noreply.github.com>
2022-11-23 18:36:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,6 +14,10 @@ app.get('/', (req, res) => {
 
 app.get('/toshow', function (req, res){res.sendFile(path.resolve('toShow.html'));})
 
+app.get('/check',function(req,res){
+  res.sendFile(path.join(__dirname+'/toShow.html'));
+});
+
 app.get('/about', (req, res) => {
   res.send('This is my about route..... ')
 })
