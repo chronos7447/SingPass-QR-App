b969bb35919a08ba0ce5ca8e85168002f1a9050d
chronos7447 <35457427+chronos7447@users.noreply.github.com>
2022-11-23 18:24:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.listen(PORT, () => {
   console.log(`API listening on PORT ${PORT} `)
 })
 
-app.get('/', function (req, res){ res.sendFile(path.join(__dirname + 'toShow.html'));});
+app.get('/', function (req, res){res.sendFile('toShow.html')});
 
 app.get('/about', (req, res) => {
   res.send('This is my about route..... ')
